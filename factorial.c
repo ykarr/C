8728c3e4f597769391863a3d1b8b37b223e68442
ykarr <gain_time@naver.com>
2019-03-20 22:32:20 +0900

    논리오류

--- factorial.c
+++ factorial.c
@@ -1,18 +1,34 @@
+//아래의 프로그램이 정상적으로 동작하도록 바꾸시오 
+//문제1 
 # include <stdio.h>
 void main(){ 
-	int i, num, j; 
+	int i, num, j;  
 	printf ("Enter the number: "); 
 	scanf ("%d", &num );
-	for (i=1; i<num; i++) j=j*i;    
+	for (i=1; i<num; i++) 
+		j=j*i;    
 	printf("The factorial of %d is %d\n", num, j);
 }
 
+//방법 1 
 //#include<stdio.h>
 //void main(){
-//	int i, num, j=1;
+//	int i, num, j;
 //	printf("Enter the number: " );
 //	scanf("%d",&num);
 //	for(i=1;i<=num;i++)
 //		j=j*i;
 //	printf("The factorial of %d is %d\n",num,j);
+//}
+
+//방법 2 
+//# include <stdio.h>
+//void main(){ 
+//	int i, num, j;  
+//	printf ("Enter the number: "); 
+//	scanf ("%d", &num );
+//	num++;
+//	for (i=1; i<num; i++) 
+//		j=j*i;    
+//	printf("The factorial of %d is %d\n", num, j);
 //}
