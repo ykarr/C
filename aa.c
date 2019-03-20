66851d69e2e6520d72cfecc4ca0bbdde5bb5fae5
ykarr <gain_time@naver.com>
2019-03-20 21:36:46 +0900

    ee
    hh

---
+++ aa.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+void main(){ 
+	int i, num, j; 
+	printf ("Enter the number: "); 
+	scanf ("%d", &num );
+	for (i=1; i<num; i++) j=j*i;    
+	printf("The factorial of %d is %d\n", num, j);
+}
+
+//#include<stdio.h>
+//void main(){
+//	int i, num, j=1;
+//	printf("Enter the number: " );
+//	scanf("%d",&num);
+//	for(i=1;i<=num;i++)
+//		j=j*i;
+//	printf("The factorial of %d is %d\n",num,j);
+//}
